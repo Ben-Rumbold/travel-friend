12d6f49ae245aefab881ef11e96129a6e91ba7e5
HannahLas <129312946+HannahLas@users.noreply.github.com>
2024-03-03 18:37:07 +0000

    Css styles added to contact page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import "./About.css";
 import Nav from "../../components/Nav/Nav";
+import imageMission from "../../assets/images/mission.jpg"; // Import your image file
+
 
 const About = () => {
   return (
     <>
       <Nav />
-      <div>
+      <div  className="container" >
         <h2>About Us</h2>
         <p>
           Welcome to Travel Friend, your go-to destination for seamless travel experiences! At Travel Friend, we understand that the key to an unforgettable journey lies not only in the destination but also in the comfort and convenience of your
@@ -15,12 +17,14 @@ const About = () => {
         </p>
 
         <h3>Our Mission</h3>
+        <img src={imageMission} alt="Mission" />
         <p>
-          Our mission is to simplify your travel planning by providing a one-stop solution for finding the nearest hotels and restaurants in your desired location. We believe that every traveler deserves a hassle-free experience, and that's exactly
+          Our mission is to simplify your travel planning by providing a one-stop solution for finding the nearest hotels and restaurants in your desired location. We believe that every traveller deserves a hassle-free experience, and that's exactly
           what we aim to deliver.
         </p>
-
-        <h3>What Sets Us Apart</h3>
+<div class ="flex-container">
+  <div class ="card">
+        <h3>What We Offer </h3>
         <ul>
           <li>
             <strong>Precision in Location:</strong> We pride ourselves on the accuracy of our search results...
@@ -32,7 +36,8 @@ const About = () => {
             <strong>User-Friendly Interface:</strong> Navigating through our website is as easy as a breeze...
           </li>
         </ul>
-
+</div>
+<div class ="card">
         <h3>How It Works</h3>
         <ol>
           <li>
@@ -42,10 +47,11 @@ const About = () => {
             <strong>Compare:</strong> Browse through a curated list of the nearest hotels and restaurants...
           </li>
           <li>
-            <strong>Book with Confidence:</strong> Once you've made your decision, book your accommodations or reserve a table at a restaurant directly through our platform...
+            <strong>Book with Confidence:</strong> Once you've made your decision, you can book directly through our platform...
           </li>
         </ol>
-
+        </div>
+        </div>
         <h3>Our Commitment to You</h3>
         <p>
           At Travel Friend, we are committed to enhancing your travel experience. Whether you're a solo adventurer, a family on vacation, or a business traveler, we understand the importance of finding the right place to stay and dine. Trust us to be
