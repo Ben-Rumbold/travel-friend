b0e11a5931ad9acbb0e7d8579362da03ac4eb95a
Shkendi <nshkendi@gmail.com>
2024-03-06 19:36:25 +0000

    updated the readme, added screenshots

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Header.css";
+import Input from "../Input/Input";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1>Travel Friend</h1>
+      <Input />
+    </div>
+  );
+};
+
+export default Header;
